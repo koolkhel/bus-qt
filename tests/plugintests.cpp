3f8745f875fa547e0043db72a700d48ec17b27b7
Sanchir Kartiev <ksb0891@gmail.com>
2015-06-05 11:05:44 +0300

    fixes with fileNames of path

--- tests/plugintests.cpp
+++ tests/plugintests.cpp
@@ -15,9 +15,9 @@
 #include <google/protobuf/io/zero_copy_stream_impl_lite.h>
 #include <google/protobuf/text_format.h>
 
-#include "events.pb.h"
-#include "geo.pb.h"
-#include "indigo.pb.h"
+#include "events_message.pb.h"
+#include "geo_message.pb.h"
+#include "indigo_message.pb.h"
 
 #include "module.h"
 #include "pluginmodulefactory.h"
